b208469729d4e9e9d76f676f45c264ad5ec35756
chops318 <chops318@gmail.com>
2016-09-11 22:38:14 -0700

    Added tests

--- lab-edward-smith/test/tcpTest.js
+++ lab-edward-smith/test/tcpTest.js
@@ -1,2 +1,19 @@
+'use strict';
+
 const expect = require('chai').expect;
 const net    = require('net');
+
+const server = require('../server');
+
+describe('Chat Server', () => {
+  it('creates a server', function() {
+    expect(server).to.exist;
+  });
+
+  it('should have 2 clients', function() {
+    this.client1 = net.connect(3000);
+    this.client2 = net.connect(3000);
+    expect(this.client1).to.exist;
+    expect(this.client2).to.exist;
+  });
+});
